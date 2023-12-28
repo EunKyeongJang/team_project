c631844ed452a511839429db1878e4a803d6b468
kkuljam <adgrvet13@naver.com>
2023-12-29 00:28:01 +0900

    12:27

--- admin/orderlist/연습용/main.js
+++ admin/orderlist/연습용/main.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded',function(){
     let cartArray = JSON.parse(localStorage.getItem('cartArray'));
     if( cartArray == null ){ cartArray = [] };
 
-    const cart ={pno:2, count:1}
+    const cart ={pno:1, count:2}
     cartArray.push(cart);
     localStorage.setItem('cartArray',JSON.stringify(cartArray));
 
-    // let productArray= JSON.parse(localStorage.getItem('productArray'));
-    // if( productArray == null ){ productArray = [] };
-
-    // const product ={pno:3, pname:'블랙바비큐콰트로치즈와퍼', pprice:13500, pimg:'블랙바비큐콰트로치즈와퍼.png', cno:1}
-    // productArray.push(product);
-    // localStorage.setItem('productArray',JSON.stringify(productArray));
-    // [
-    //     {pno:1, pname:'몬스터와퍼', pprice:9500, pimg:'몬스터와퍼.png', cno:3},
-    //     {pno:2, pname:'블랙바비큐와퍼', pprice:10500, pimg:'블랙바비큐와퍼.png', cno:2},
-    //     {pno:3, pname:'블랙바비큐콰트로치즈와퍼', pprice:13500, pimg:'블랙바비큐콰트로치즈와퍼.png', cno:1},
-    // ]
+     let productArray= JSON.parse(localStorage.getItem('productArray'));
+     if( productArray == null ){ productArray = [] };
+/*
+     const product ={pno:3, pname:'블랙바비큐콰트로치즈와퍼', pprice:13500, pimg:'블랙바비큐콰트로치즈와퍼.png', cno:1}
+     productArray.push(product);
+     localStorage.setItem('productArray',JSON.stringify(productArray));
+     [
+         {pno:1, pname:'몬스터와퍼', pprice:9500, pimg:'몬스터와퍼.png', cno:3},
+         {pno:2, pname:'블랙바비큐와퍼', pprice:10500, pimg:'블랙바비큐와퍼.png', cno:2},
+         {pno:3, pname:'블랙바비큐콰트로치즈와퍼', pprice:13500, pimg:'블랙바비큐콰트로치즈와퍼.png', cno:1},
+     ]*/
 })
