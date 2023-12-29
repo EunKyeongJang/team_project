c261aa6b6d6080a404443ffc14db6a620814f049
hyo-s <sere4580@naver.com>
2023-12-29 15:31:39 +0900

    20231229_hyo_test

--- admin/common.js
+++ admin/common.js
@@ -1,20 +1,3 @@
-/* $(document).ready(function(){
+$(document).ready(function(){
     $('#header').load('/admin/admin.html');
-}); */
-
-
-document.addEventListener('DOMContentLoaded',function(){
-    categoryPrint(1);
-})
-
-    let adminCategory=['주문내역','매출조회','제품관리']; console.log(adminCategory);
-
-    function categoryPrint(index){
-    const adminBox=document.querySelector('#adminBox')
-    let html='';
-        for(let i=0; i<adminCategory.length; i++){
-            html+=
-            `<div onclick="categoryPrint(${adminCategory[i]})" id="${adminCategory[i]==index?'selectBox':''}">${adminCategory[i]}</div>`
-        }
-    adminBox.innerHTML=html;
-    }
+});
