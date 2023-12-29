3c701e40cbdb296ab36f06401c0eac0bf72d9676
kkuljam <adgrvet13@naver.com>
2023-12-29 13:33:10 +0900

    13:33

--- admin/orderlist/orderlist.js
+++ admin/orderlist/orderlist.js
@@ -4,7 +4,7 @@
         카트     :[{메뉴번호,개수},{메뉴번호,개수},{메뉴번호,개수}]
         주문내역 :[{주문번호,메뉴번호,주문날짜,주문상태, 총가격, 개수}]
 */
-
+/*
 let orderArray = JSON.parse(localStorage.getItem('orderArray')); //localStorage에 orderArray 배열 만듦
 if( orderArray == null ){ orderArray = [] };
 
@@ -17,6 +17,13 @@ function order(){// 주문하기 버튼 눌렀을 때 실행될 함수
         //cartArray 있는 배열의 객체마다 리스트 번호를 추가
         cartArray[i].list=list
         let cart=cartArray[i]
+        for(let j=0; j<productArray.length;j++){
+            if(cartArray[i].pno==productArray[j].pno){
+                cartArray[i].list=list
+                cartArray[i].date=new Date().toLocaleDateString()
+                cartArray[i].total=productArray[j].pprice*orderArray[i].count
+            }
+        }
         //객체 개별로 cart에 저장
       
         orderArray.push(cart) //order 배열에 추가
@@ -28,61 +35,66 @@ function order(){// 주문하기 버튼 눌렀을 때 실행될 함수
     localStorage.setItem('cartArray',JSON.stringify(cartArray)); // 로컬에도 초기화 저장 
     orderlist();
  }
-
-//--------[함수2] 출력함수------------------
-function orderlist(){
-    let productArray = JSON.parse(localStorage.getItem('productArray')); //로컬에서 불러오기
-    let orderArray = JSON.parse(localStorage.getItem('orderArray'));
-   
-    const orderlist = document.querySelector('tbody') //어디에
-    let html=``;
-   
-    for(let i=0; i<orderArray.length;i++){ // order 배열에 있는거 다 출력
-        for(let j=0; j<productArray.length; j++){ // 제품 배열 반복
-            if(orderArray[i].pno==productArray[j].pno){ // order 배열의 pno과 제품 pno 일치 찾기
-                // 만약 객체에 status 값이 있다면 select 값을 그걸로 출력하기
-                html+=` <tr>
-                            <th>${orderArray[i].list}</th> 
-                            <th>${productArray[j].pname}</th>
-                            <th>${new Date().toLocaleDateString()}</th> 
-                            <th>
-                               <select 
-                                    name="${orderArray[i].list}:${orderArray[i].pno}" 
-                                    onchange="status(${orderArray[i].list}, ${orderArray[i].pno})">
-                                        <option>대기중</option>
-                                        <option>취소</option>
-                                        <option>완료</option>
-                                </select>
-                            </th>
-                            <th>${productArray[j].pprice*orderArray[i].count}</th>
-                            <th>${orderArray[i].count}</th>
-                            
-                         </tr>`
-
-                orderArray[i].date=new Date().toLocaleDateString()
-                orderArray[i].total=productArray[j].pprice*orderArray[i].count
-                localStorage.setItem('orderArray',JSON.stringify(orderArray)); // 로컬 배열에도 추가
-                console.log(j)
+ */
+document.addEventListener('DOMContentLoaded',function(){
+    //--------[함수2] 출력함수------------------
+    orderlist();
+    function orderlist(){
+        let productArray = JSON.parse(localStorage.getItem('productArray')); //로컬에서 불러오기
+        let orderArray = JSON.parse(localStorage.getItem('orderArray'));
+       
+        const orderlist = document.querySelector('tbody') //어디에
+        let html=``;
+       
+        for(let i=0; i<orderArray.length;i++){ // order 배열에 있는거 다 출력
+            for(let j=0; j<productArray.length; j++){ // 제품 배열 반복
+                if(orderArray[i].pno==productArray[j].pno){ // order 배열의 pno과 제품 pno 일치 찾기
+                    // 만약 객체에 status 값이 있다면 select 값을 그걸로 출력하기
+                    html+=` <tr>
+                                <th>${orderArray[i].list}</th> 
+                                <th>${productArray[j].pname}</th>
+                                <th>${new Date().toLocaleDateString()}</th> 
+                                <th>
+                                   <select 
+                                        name="${orderArray[i].list}:${orderArray[i].pno}" 
+                                        onchange="status(${orderArray[i].list}, ${orderArray[i].pno})">
+                                            <option>대기중</option>
+                                            <option>취소</option>
+                                            <option>완료</option>
+                                    </select>
+                                </th>
+                                <th>${productArray[j].pprice*orderArray[i].count}</th>
+                                <th>${orderArray[i].count}</th>
+                                
+                             </tr>`
+    
+                    orderArray[i].date=new Date().toLocaleDateString()
+                    orderArray[i].total=productArray[j].pprice*orderArray[i].count
+                    localStorage.setItem('orderArray',JSON.stringify(orderArray)); // 로컬 배열에도 추가
+                    console.log(j)
+                }
             }
         }
+        orderlist.innerHTML=html; 
+
     }
-    orderlist.innerHTML=html; 
-}
 
-// -----------[함수3] 상태 저장 후 배열 객체에 넣기------------------
-
+});
+  // -----------[함수3] 상태 저장 후 배열 객체에 넣기------------------
 function status(list, pno){
     console.log('상태함수')
-     let orderArray = JSON.parse(localStorage.getItem('orderArray'));
-     for(let i=0; i<orderArray.length; i++){
-         if(orderArray[i].list==list && orderArray[i].pno==pno){ 
-             orderArray[i].status=document.querySelector(`select[name='${list}:${pno}']`).value;
-             localStorage.setItem('orderArray',JSON.stringify(orderArray));
-             console.log(orderArray)
-         }
-     }
+    let orderArray = JSON.parse(localStorage.getItem('orderArray'));
+    for(let i=0; i<orderArray.length; i++){
+        if(orderArray[i].list==list && orderArray[i].pno==pno){ 
+            orderArray[i].status=document.querySelector(`select[name='${list}:${pno}']`).value;
+            localStorage.setItem('orderArray',JSON.stringify(orderArray));
+            console.log(orderArray)
+        }
+    }
     
- }
+}
+
+
 /*
 function status(list){
    console.log('상태함수')
